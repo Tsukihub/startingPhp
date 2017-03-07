c01fd4b1b2bd0c2a6768dffa7fa438c08fba9ef5
Tsukihub <audeeccher@gmail.com>
2017-03-07 13:50:29 +0100

    exercice 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>S</title>
+</head>
+<body>
+<head>
+	
+</head>
+<?php include 'variable.php' ?>	
+<footer>
+	
+</footer>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,4 @@
+<?php
+$nom = 5;
+echo $nom;
+?>
