725ed7e01cb9a34d58c9d457df0628390b7924ca
Tsukihub <audeeccher@gmail.com>
2017-03-08 13:59:13 +0100

    ex5.4.5.6

--- tableau.php
+++ tableau.php
@@ -14,6 +14,24 @@ $mois = array("janvier", "fevrier", "mars", "avril",  "mai", "juin",  "juillet",
 echo $mois[2]."<br/>";
 echo "ex 3 :<br/> ";
 echo $mois[5]."<br/>";
+$mois[3]="julien";
+echo "ex 4 :<br/> ";
+echo $mois[3]."<br/>";
+
+echo "ex 5 :<br/> ";
+$hautDeFrance=array(
+    02 => "Aisne",
+    59 => "Nord",
+    60 => "Oise",
+    62 => "Pas-de-Calais",
+    80 => "Somme"
+);
+
+var_dump($hautDeFrance);
+echo "<br/> ";
+echo "ex 6 :<br/> ";
+echo $hautDeFrance[59]."<br/>";
+echo "ex 7 :<br/> ";
 
 ?>
 </div>
