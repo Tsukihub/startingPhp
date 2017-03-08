6187cff80ab2115705761918135be8b31361c6e6
Tsukihub <audeeccher@gmail.com>
2017-03-08 11:22:13 +0100

    return string

--- fonction.php
+++ fonction.php
@@ -9,10 +9,22 @@
 <h1>Boucle la</h1>
 
 <?php
+echo "<div>";
 function True(){
 	return true;
 }
 echo True();
+
+echo "<br/>";
+
+$string="jean";
+
+function True2($string){
+	return $string;
+}
+echo True2($string);
+
+
 echo "<br/>";
 
 // $var1=0;
@@ -20,7 +32,7 @@ echo "<br/>";
 // while ($var1 <=20){
 // 	echo ("<br/>".($var1*$var2));
 // 	$var1 ++;
-
+echo "</div>";
 ?>
 
 </body>
---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+include 'fonction.php';
+$string="test";
+if (True()== true){
+	echo "exo1 ok";
+
+	if (True2($string)=="jean"){
+	echo "exo2 ok";
+
+		if (){
+			echo "exo3 ok";
+		}else{
+			echo ("exo 3 failed");
+		}
+
+
+	}else{
+	echo ("exo 2 failed");
+	}
+
+}else{
+	echo ("exo 1 failed");
+}
+
+?>
