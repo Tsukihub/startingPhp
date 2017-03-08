916566ad36ef283ddee7e3e8c7a73178f163bbb7
Tsukihub <audeeccher@gmail.com>
2017-03-08 16:51:00 +0100

    7.6

--- user.php
+++ user.php
@@ -15,12 +15,52 @@
 
 
 if(isset($_POST['nom']) AND isset($_POST['prenom'])) {
+	if($_POST['nom']!=="" AND $_POST['prenom']!==""){
 	    
 		    $testA = $_POST['nom'];
 		    $testB = $_POST['prenom'];
 		    echo "POST bienvenue ".$testA." ".$testB."<br/>";
+	}else{
+	echo " <form action='user.php' method='post'>
+      <fieldset>
+        <legend>Post</legend>
+          <p>
+             <label for='prenom'>Prenom</label> 
+            <input type='text' name='prenom' id='prenom'>
+          </p>
+          <p>
+            <label for='nom'>Nom :</label> 
+            <input type='text' name='nom' id='nom' /> 
+            <input type='submit' name='submit' value='Envoyer' />
+          </p>
+          <SELECT name='nom' size='1'>
+			<OPTION>mr
+			<OPTION>mme
+		  </SELECT>
+      </fieldset>
+    </form>";
+}
 	
 
+}else{
+	echo " <form action='user.php' method='post'>
+      <fieldset>
+        <legend>post</legend>
+          <p>
+             <label for='prenom'>Prenom</label> 
+            <input type='text' name='prenom' id='prenom'>
+          </p>
+          <p>
+            <label for='nom'>Nom :</label> 
+            <input type='text' name='nom' id='nom' /> 
+            <input type='submit' name='submit' value='Envoyer' />
+          </p>
+          <SELECT name='nom' size='1'>
+			<OPTION>mr
+			<OPTION>mme
+		  </SELECT>
+      </fieldset>
+    </form>";
 }
 
 ?>
