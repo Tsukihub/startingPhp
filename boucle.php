9176f0ec7a3310eac447b59629d4c6a683065c7c
Tsukihub <audeeccher@gmail.com>
2017-03-08 10:15:01 +0100

    boucle un demi valeuret boucle décrémenter

--- boucle.php
+++ boucle.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>boucles</title>
+</head>
+<body>
+	
+
 <?php
 $zeroadix=0;
 while ( $zeroadix < 10) {
@@ -15,4 +24,24 @@ while ($var1 <=20){
 	$var1 ++;
 }
 
+
+echo "<br/>";
+
+$var3=100;
+// $var2=1;
+while ($var3>10){
+	echo ("<br/>".($var3*$var2));
+	$var3 --;
+}
+
+echo "<br/>";
+$incrementdemi=1;
+while ($incrementdemi<=10){
+	echo ("<br/>".($incrementdemi));
+	$incrementdemi=$incrementdemi+($incrementdemi/2);
+}
+
 ?>
+
+</body>
+</html>
